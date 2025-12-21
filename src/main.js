e7e3855c2a377de0d22bad8233dfd1573fb725cf
Yuichi Yazaki <visualizingjp@gmail.com>
2025-12-21 10:16:48 +0900

    ファイルの読み込み方を以前に戻す

--- src/main.js
+++ src/main.js
@@ -5,8 +5,5 @@ import './nav.css';
 // Scripts
 import './project_id_rescue.js';
 import './constants.js';
-import './supabase.js';
-import './dataviz-auth-client.js';
-import './sankey.js';
-import './cloud_api.js';
+
 import './sankeymatic.js';
